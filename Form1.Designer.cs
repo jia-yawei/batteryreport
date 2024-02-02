9508dd3fa14ff1224e06ec3af68ebfdf88035393
jiayw <jiayw530@163.com>
2024-02-02 17:02:04 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace batteryreport
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            dataGridView1 = new DataGridView();
+            datagridview = new DataGridViewTextBoxColumn();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(318, 40);
+            button1.Name = "button1";
+            button1.Size = new Size(138, 34);
+            button1.TabIndex = 0;
+            button1.Text = "获取电池信息";
+            button1.UseVisualStyleBackColor = true;
+           //绑定button1_Click事件
+           button1.Click += new EventHandler(button1_Click);
+
+
+
+            
+            
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { datagridview, Column1, Column2, Column3 });
+            dataGridView1.Location = new Point(116, 126);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.Size = new Size(575, 59);
+            dataGridView1.TabIndex = 1;
+            // 
+            // datagridview
+            // 
+            datagridview.AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            datagridview.HeaderText = "设计容量";
+            datagridview.MinimumWidth = 8;
+            datagridview.Name = "datagridview";
+            datagridview.Width = 118;
+            // 
+            // Column1
+            // 
+            Column1.AutoSizeMode = DataGridViewAutoSizeColumnMode.ColumnHeader;
+            Column1.HeaderText = "当前最大容量";
+            Column1.MinimumWidth = 8;
+            Column1.Name = "Column1";
+            Column1.Width = 154;
+            // 
+            // Column2
+            // 
+            Column2.AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            Column2.HeaderText = "循环次数";
+            Column2.MinimumWidth = 8;
+            Column2.Name = "Column2";
+            Column2.Width = 118;
+            // 
+            // Column3
+            // 
+            Column3.AutoSizeMode = DataGridViewAutoSizeColumnMode.ColumnHeader;
+            Column3.HeaderText = "损耗比例";
+            Column3.MinimumWidth = 8;
+            Column3.Name = "Column3";
+            Column3.Width = 118;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 24F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(796, 251);
+            Controls.Add(dataGridView1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "电池信息";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+
+      
+
+        #endregion
+
+        private Button button1;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn datagridview;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System.Data;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+namespace batteryreport
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process process_cmd = new Process();
+            process_cmd.StartInfo.FileName = "cmd.exe";//进程打开文件名为“cmd”
+            process_cmd.StartInfo.RedirectStandardInput = true;//是否可以输入
+            process_cmd.StartInfo.RedirectStandardOutput = true;//是否可以输出
+            process_cmd.StartInfo.UseShellExecute = false;//是否使用操作系统shell启动
+            process_cmd.StartInfo.CreateNoWindow = true;//是否在新窗口中启动进程
+            process_cmd.Start();//启动进程
+            process_cmd.StandardInput.WriteLine("powercfg /batteryreport");//输入命令
+            Thread.Sleep(1000);//等待1秒   
+            //获取电池报告的路径，这个路径在当前exe文件的同级目录下
+            string batteryreport_path = System.IO.Directory.GetCurrentDirectory() + "\\battery-report.html";
+            //使用HtmlAgilityPack解析html文件 
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.Load(batteryreport_path);
+            //获取电池报告的表格,在html文件第二个表格
+            HtmlNode table = doc.DocumentNode.SelectSingleNode("//table[2]");
+            //对表格进行处理，将表格中第4、6、7行，第2列的数据提取出来，依次放到dataGridView1第1行
+            int i = 0;
+            int k = 0;
+            foreach (HtmlNode row in table.SelectNodes("tr"))
+            {
+                if (i == 4 || i == 6 || i == 7)
+                {
+                    int j = 0;
+                    foreach (HtmlNode cell in row.SelectNodes("td"))
+                    {
+                        if (j == 1)
+                        {
+                            dataGridView1.Rows[0].Cells[k].Value = cell.InnerText;
+                        }
+                        j++;
+                    }
+                    k++;
+                }
+                i++;
+
+            }
+
+            //将第一列的值减去第二列的值，除以第一列的值，得到电池的损耗率             
+            string str1 = dataGridView1.Rows[0].Cells[0].Value.ToString();
+            string pattern1 = @"[^\d]";
+            string result1 = Regex.Replace(str1, pattern1, "");
+            string str2 = dataGridView1.Rows[0].Cells[1].Value.ToString();
+            string pattern2 = @"[^\d]";
+            string result2 = Regex.Replace(str2, pattern2, "");
+            double result1_double = Convert.ToDouble(result1);
+            double result2_double = Convert.ToDouble(result2);
+            double result_double = (result1_double - result2_double) / result1_double;
+            dataGridView1.Rows[0].Cells[3].Value = result_double.ToString("P");
+        }
+
+
+    }
+}
